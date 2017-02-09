da49dd640ffa537e093eb1032e6be450d8634061
Mia Yao <anqiyao.90@gmail.com>
2017-02-09 18:44:56 -0500

    Delete changeUserAgent.m

--- changeUserAgent.m
+++
@@ -1,5 +0,0 @@
-[webView evaluateJavaScript: @"navigator.userAgent" completionHandler: ^(id result, NSError *error){
-    NSString *oldAgent = result;
-    NSString *newAgent = [NSString stringWithFormat:@"%@;***",oldAgent];
-    webView.customUserAgent = newAgent;
-}];
